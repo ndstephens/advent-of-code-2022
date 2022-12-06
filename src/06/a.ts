9237857ddd86b658d6e2ef934ae4e9c0b3e20dc6
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2022-12-06 13:43:26 -0500

    day 6

---
+++ src/06/a.ts
@@ -0,0 +1,35 @@
+import { readFileSync } from 'fs';
+
+const input = readFileSync('./input.txt', 'utf-8');
+
+//* EXAMPLE SIGNALS
+const ex1 = 'mjqjpqmgbljsphdztnvjfqwrcgsmlb';
+const ex2 = 'bvwbjplbgvbhsrlpgdmjqwftvncz';
+const ex3 = 'nppdvjthqldpwncqszvftbrmjlhg';
+const ex4 = 'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg';
+const ex5 = 'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw';
+
+const subroutine = (signal: string, packetLength: number) => {
+  let packetFound = false;
+  let position = 0;
+
+  while (!packetFound) {
+    if (
+      [...new Set([...signal.slice(position, position + packetLength)])]
+        .length === packetLength
+    ) {
+      packetFound = true;
+    } else {
+      position++;
+    }
+  }
+  return position + packetLength;
+};
+
+//* PART ONE
+console.log(subroutine(input, 4));
+// 1760
+
+//* PART TWO
+console.log(subroutine(input, 14));
+// 2974
